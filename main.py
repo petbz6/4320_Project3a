d573c8c280fa78e7693170ceff78c73dd25faeab
petbz6 <92735934+petbz6@users.noreply.github.com>
2024-04-11 13:46:24 -0500

    Added function to read first column of the stocks.csv file and get the stock symbols

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import stock_query
 import date
 import graph
 
-
 while True:
     print("Stock Data Visualizer")
     print("--------------------------- \n")
@@ -15,5 +14,15 @@ while True:
     graph.generate_graph(data, chart_type, time_series, start, end)
 
     response = input("Would you like to view more stock data? Press 'y' to continue: ")
-    if response != 'y':
+    if response != "y":
         break
+
+# Reads the first column of the stocks.csv file to get the stock symbols
+def get_stock_symbols():
+    stock_symbols = []
+    with open("stocks.csv", newline='') as file:
+        csvFile = csv.reader(file)
+        next(csvFile)
+        for row in csvFile:
+            stock_symbols.append(row[0])
+    return stock_symbols
